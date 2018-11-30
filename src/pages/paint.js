689e03d5b64924956bfa75084c8b4ecc9f3f556d
Noah K <nkernis@gmail.com>
2018-11-30 16:26:05 -0500

    added 'secret' page and changed auido to paint

--- src/pages/paint.js
+++ src/pages/paint.js
@@ -1,22 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import withRoot from '../withRoot';
+import AsyncComponent from '../components/asyncComponent.js'
 
-const styles = theme => ({});
+const styles = theme => ({
+  root: {
+    paddingLeft: theme.spacing.unit * 1.2
+  },
+  headers: {
+    marginTop: theme.spacing.unit * 1.2,
+    marginBottom: theme.spacing.unit * 1.2
+  }
+})
 
 class Paint extends React.Component {
-  render() {
+  state = {
+    baseURL: 'https://s3.amazonaws.com/images.noahkernis.com/paint/',
+    images: [
+      'portrait_2018.png',
+      'hecho_en_america_2018.png',
+      'fearing_what_comes_next_2018.png'
+    ]
+  }
 
+  returnImages = () => {
+    const { baseURL, images } = this.state
+
+    return images.map((imageName, i) => {
+      return (
+        <AsyncComponent
+          key={i}
+          baseURL={baseURL}
+          imageName={imageName}
+          index={i} />
+      )
+    })
+  }
+
+  render() {
+    const { classes } = this.props
     return (
-      <div>
-        "PAINT"
+      <div className={classes.root}>
+        <Typography variant="h6" className={classes.headers}>
+          Paintings
+        </Typography>
+        { this.returnImages() }
       </div>
     );
   }
 }
 
-// Paint.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
+Paint.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default withStyles(styles)(Paint)
+export default withRoot(withStyles(styles)(Paint))
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import NoahKernis from './pages/noahKernis.js'
 import Code from './pages/code.js'
 import Draw from './pages/draw.js'
 import Paint from './pages/paint.js'
+import SuperSecretPage from './pages/superSecretPage.js'
 import Write from './pages/write.js'
 import GenericError from './pages/genericError.js'
 
@@ -17,7 +18,8 @@ export default () => {
       <Route path="/" exact component={NoahKernis} props={childProps} />
       <Route path="/code" exact component={Code} props={childProps} />
       <Route path="/draw" exact component={Draw} props={childProps} />
-      <Route path="/audio" exact component={Paint} props={childProps} />
+      <Route path="/paint" exact component={Paint} props={childProps} />
+      <Route path="/super_secret_page" exact component={SuperSecretPage} props={childProps} />
       <Route path="/write" exact component={Write} props={childProps} />
       <Route path="/generic_error" exact component={GenericError} props={childProps} />
     </Switch>
